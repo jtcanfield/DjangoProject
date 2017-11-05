27705fd2f0aeda163e99790bd428ff1aecdf5c66
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-11-05 11:53:15 -0500

    import in views

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
 def greeting_view(request):
-    return HttpResponse("hellow world")
+    return HttpResponse("Hello World!")
